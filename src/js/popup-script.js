efc077dba68301624f9dfe1e5c831377cfc500e2
Anton Chushialov <antonn.chushialov@gmail.com>
2021-06-17 17:49:46 +0400

    added cookie logic

--- src/js/popup-script.js
+++ src/js/popup-script.js
@@ -1,3 +1,11 @@
+onload = () => {
+    var cookieUsername = getCookie('username');
+
+    if (cookieUsername) {
+        successLogin(cookieUsername);
+    }
+}
+
 const popup = document.querySelector('.popup');
 
 function showPopup() {
@@ -9,6 +17,7 @@ function hidePopup() {
 }
 
 var btn = document.getElementById('log-btn');
+var checkbox = document.getElementById('checkbox-remember');
 var divName = document.getElementById('div-name-content');
 
 function clearLoginInput() {
@@ -31,12 +40,7 @@ function login() {
     for (let key in hash) {
         if (key === username) {
             if (atob(hash[key]) === password) {
-                btn.classList.remove('header-button-back');
-                btn.classList.add('header-button');
-                btn.onclick = logout;
-                btn.innerHTML = "Выйти";
-                divName.innerHTML = username;
-                hidePopup();
+                successLogin(username);
             } else {
                 alert('пароль неверен');
             }
@@ -49,7 +53,7 @@ function login() {
     a.push({ name: username, password: btoa(password) });
 
     localStorage.setItem('all_users', JSON.stringify(a));
-    hidePopup();
+    successLogin(username);
     clearLoginInput();
 }
 
@@ -59,4 +63,44 @@ function logout() {
     btn.innerHTML = "Войти";
     btn.onclick = showPopup;
     divName.innerHTML = '';
+    eraseCookie('username');
+}
+
+function successLogin(username) {
+    if (checkbox.checked) {
+        setCookie('username', username, 1);
+        console.log('saved');
+    }
+    btn.classList.remove('header-button-back');
+    btn.classList.add('header-button');
+    btn.onclick = logout;
+    btn.innerHTML = "Выйти";
+    divName.innerHTML = username;
+    hidePopup();
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    console.log(document.cookie);
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
 }
